99503a467a093559dccf8d660a02dacceee9a037
falcondamian <manuelfalcondamian@gmail.com>
2015-07-22 20:37:23 +0200

    Creación de comentarios

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,7 +3,14 @@ var models = require('../models/models.js');
 // funcion de load
 exports.load = function(req, res, next, quizId) {
 
-	models.Quiz.findById(quizId).then(function (quiz) {
+	models.Quiz.find({
+            where: {
+                id: Number(quizId)
+            },
+            include: [{
+                model: models.Comment
+            }]
+        }).then(function (quiz) {
 		if (quiz) {
 			req.quiz = quiz;
 			next();
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+//Definicion rutas quizes
 //Autoload de quiz con quizId
 router.param('quizId', quizController.load);
 
@@ -23,5 +25,9 @@ router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 router.get('/quizes/author', function(req, res) {
   res.render('author');
 });
+
+//Definicion rutas comments
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
 module.exports = router;
